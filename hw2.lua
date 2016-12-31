2a1d9472abe4686cd127b929c58947ba2abbee53
shadaryr <shadaryr@campus.technion.ac.il>
2016-12-31 12:11:20 +0200

    w

--- hw2.lua
+++ hw2.lua
@@ -48,11 +48,82 @@ for i=1,3 do -- over each image channel
     testData[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
 end
 
+--  ****************************************************************
+--  data augmentation
+--  ****************************************************************
 
+do -- data augmentation module
+  local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
+
+  function BatchFlip:__init()
+    parent.__init(self)
+    self.train = true --so the data augmentation will only happen in the training phase!!!
+  end
+
+  function BatchFlip:updateOutput(input)
+    if self.train then
+      local permutation = torch.randperm(input:size(1))
+      for i=1,input:size(1) do
+        if 0 == permutation[i] % 3  then f(input[i]) end -- need to define f (f= a data augmentation method)
+		if 1 == permutation[i] % 3  then g(input[i]) end -- need to define g (g= a data augmentation method)
+      end -- and if 2== %3 -> do nothing.
+    end -- in the expectancy - will train on a 3 times bigger set than the original training set, without really saving all augmented data! just by doing a lot of epochs - beacuse we do this in each epoch for each image all over again
+    self.output:set(input:cuda())
+    return self.output
+  end
+end
+ 
+local function hflip(x)
+--[[
+Flips image src horizontally (left<->right). 
+If dst is provided, it is used to store the output image. 
+Otherwise, returns a new res Tenso
+]]
+   return torch.random(0,1) == 1 and x or image.hflip(x)
+end
+
+local function randomcrop(im , pad, randomcrop_type)
+   if randomcrop_type == 'reflection' then
+      -- Each feature map of a given input is padded with the replication of the input boundary
+      module = nn.SpatialReflectionPadding(pad,pad,pad,pad):float() 
+   elseif randomcrop_type == 'zero' then
+      -- Each feature map of a given input is padded with specified number of zeros.
+	  -- If padding values are negative, then input is cropped.
+      module = nn.SpatialZeroPadding(pad,pad,pad,pad):float()
+   end
+	
+   local padded = module:forward(im:float())
+   local x = torch.random(1,pad*2 + 1)
+   local y = torch.random(1,pad*2 + 1)
+   --image.save('img2ZeroPadded.jpg', padded)
+
+   return padded:narrow(3,x,im:size(3)):narrow(2,y,im:size(2))
+end
+
+--[[
+-- Test hflip
+local im = image.load('img.jpg') --im will hold a tensor
+image.save('imghflip.jpg', image.hflip(im))
+
+-- Test reflection
+local im = image.load('img.jpg')
+local I = randomcrop(im, 10, 'reflection')
+print(I:size(),im:size())
+image.save('img2ReflectionCrop.jpg', I)
+
+-- Test zero padding with crop
+local im = image.load('img.jpg')
+local I = randomcrop(im, 10, 'zero')
+print(I:size(),im:size())
+image.save('img2ZeroCrop.jpg', I)
+]]
 
 --  ****************************************************************
 --  Define our neural network
 --  ****************************************************************
+-- all the calculation near the layers are the output size of the layer
+
+--model:add(nn.BatchFlip():float()) --data augmentation layer
 
 local model = nn.Sequential()
 model:add(cudnn.SpatialConvolution(3, 32, 5, 5)) -- 3 input image channel, 32 output channels, 5x5 convolution kernel. owidth=floor((32+2*0-5)/1 +1)=28. same goes to ohight. output- 28*28*32
@@ -65,10 +136,11 @@ model:add(cudnn.ReLU(true))
 model:add(nn.SpatialBatchNormalization(64))
 model:add(cudnn.SpatialConvolution(64, 32, 3, 3)) --gets 6*6*64. 32 filters. 3*3 is the surface of each kernel floor((6+2*0-3)/1 +1)*floor((6+2*0-3)/1 +1)*32=4*4*32
 model:add(nn.View(32*4*4):setNumInputDims(3))  -- reshapes from a 3D tensor of 32x4x4 into 1D tensor of 32*4*4
-model:add(nn.Linear(32*4*4, 64))             -- fully connected layer (matrix multiplication between input and weights). gets a 32*4*4 vector, outputs 64 neurons. parameters (32*4*4+1)*64 (the +1 is bias)
+--model:add(nn.Linear(32*4*4, 64))             -- fully connected layer (matrix multiplication between input and weights). gets a 32*4*4 vector, outputs 64 neurons. (32*4*4+1)*64 (the +1 is bias)
 model:add(cudnn.ReLU(true))
 model:add(nn.Dropout(0.5))                      --Dropout layer with p=0.5
-model:add(nn.Linear(64, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits) (64+1)*10
+--model:add(nn.Linear(64, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits) (64+1)*10
+model:add(nn.Linear(512, #classes))            -- 10 is the number of outputs of the network (in this case, 10 digits) (512+1)*10
 model:add(nn.LogSoftMax())                     -- converts the output to a log-probability. Useful for classificati
 
 model:cuda()
