be96434405df4c63d8e7ad2a3bbcfd6d372efe98
shadaryr <shadaryr@campus.technion.ac.il>
2017-01-03 14:48:00 +0200

    Add files via upload

--- hw2.lua
+++ hw2.lua
@@ -308,11 +308,11 @@ for e = 1, epochs do
       bestError = testError[e]
 	end
 
-local WritetrainError = trainError[e]
-local WritetrainLoss = trainLoss[e] 
-local WritetestError = testError[e]
-local WritetestLoss = testLoss[e]
-local f = assert(io.open('logFile1.log', 'a+'), 'Failed to open input file')
+	local WritetrainError = trainError[e]
+	local WritetrainLoss = trainLoss[e] 
+	local WritetestError = testError[e]
+	local WritetestLoss = testLoss[e]
+	local f = assert(io.open('logFile1.log', 'a+'), 'Failed to open input file')
 	if e > 1 then
 		print('test Error: ')
 		print(testError[e])
