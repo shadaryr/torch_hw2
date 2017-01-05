af43280cb6e590dccfdfd3738d823e7660b83841
shadaryr <shadaryr@campus.technion.ac.il>
2017-01-05 21:30:32 +0200

    with batchflip module inside

--- model_load_hw2.lua
+++ model_load_hw2.lua
@@ -34,9 +34,71 @@ function returnAvgError()
 		testData[{ {}, {i}, {}, {}  }]:div(stdv[i]) -- std scaling
 	end
 
+
 	-- tranfering to cuda
 	--testData = testData:cuda()
 	--testLabels = testLabels:cuda()
+	
+	------DATA AUGMENTATION -- BATCHFLIP-----------
+	local function hflip(x)
+	--[[
+	Flips image src horizontally (left<->right). 
+	If dst is provided, it is used to store the output image. 
+	Otherwise, returns a new res Tenso
+	]]
+	   return torch.random(0,1) == 1 and x or image.hflip(x)
+	end
+
+	local function vflip(x)
+	--[[
+	Flips image src horizontally (left<->right). 
+	If dst is provided, it is used to store the output image. 
+	Otherwise, returns a new res Tenso
+	]]
+	   return torch.random(0,1) == 1 and x or image.vflip(x)
+	end
+
+	local function randomcrop(im , pad, randomcrop_type)
+	   if randomcrop_type == 'reflection' then
+		  -- Each feature map of a given input is padded with the replication of the input boundary
+		  module = nn.SpatialReflectionPadding(pad,pad,pad,pad):float() 
+	   elseif randomcrop_type == 'zero' then
+		  -- Each feature map of a given input is padded with specified number of zeros.
+		  -- If padding values are negative, then input is cropped.
+		  module = nn.SpatialZeroPadding(pad,pad,pad,pad):float()
+	   end
+		
+	   local padded = module:forward(im:float())
+	   local x = torch.random(1,pad*2 + 1)
+	   local y = torch.random(1,pad*2 + 1)
+	   --image.save('img2ZeroPadded.jpg', padded)
+
+	   --return torch.random(0,1) == 1 and x or padded:narrow(3,x,im:size(3)):narrow(2,y,im:size(2))
+	   return padded:narrow(3,x,im:size(3)):narrow(2,y,im:size(2))
+	end
+
+	do -- data augmentation module
+	  local BatchFlip,parent = torch.class('nn.BatchFlip', 'nn.Module')
+
+	  function BatchFlip:__init()
+		parent.__init(self)
+		self.train = true --so the data augmentation will only happen in the training phase!!!
+	  end
+
+	  function BatchFlip:updateOutput(input)
+		if self.train then
+		  local permutation = torch.randperm(input:size(1))
+		  for i=1,input:size(1) do
+			local mod = permutation[i] % 4
+			if 0 == mod  then image.hflip(input[i]) end 
+			if 1 == mod  then randomcrop(input[i], 10, 'reflection') end
+			if 2 == mod  then randomcrop(input[i], 10, 'zero') end
+		  end -- and if mod ==3 -> do nothing.
+		end
+		self.output:set(input:cuda())
+		return self.output
+	  end
+	end
 
 	--load the model (the trained net)
 	model = torch.load('HW2_network_v2.t7')
